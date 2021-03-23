321f97b5e51bf32f205070f81f02416c0d2640f4
xyo26 <youxuefei@hotmail.com>
2021-03-23 22:50:05 +0100

    style header, get all fabrics axios request in stock container

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="flex-container">
-    <div>
-      <img alt="web logo" src="../assets/yinzhang.jpg" width="60" height="60" />
-    </div>
-    <div v-for="tab in tabs" :key="tab.name">
-      <router-link :to="tab.link" class="tab">
-        <div>{{ tab.name }}</div>
-      </router-link>
+  <div class="my-header">
+    <div class="flex-container">
+      <div>
+        <img
+          alt="web logo"
+          src="../assets/yinzhang.jpg"
+          width="32px"
+          height="32px"
+          style="margin: 0px 40px 0px 0px"
+        />
+      </div>
+      <div v-for="tab in tabs" :key="tab.name" class="my-tab">
+        <router-link :to="tab.link" class="my-link">
+          <div>{{ tab.name }}</div>
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -45,20 +53,42 @@ export default {
 </script>
 
 <style>
-.flex-container {
-  display: flex;
-  flex-wrap: nowrap;
-  justify-content: space-evenly;
+.my-header {
+  height: 56px;
+  padding: 4px 16px;
   background-color: white;
 }
-.flex-container > div {
-  width: 100px;
-  margin: 10px;
-  text-align: center;
-  line-height: 75px;
-  font-size: 30px;
+.flex-container {
+  height: 56px;
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: flex-start;
+  align-items: center;
+  margin: 0px 58px;
+  padding: 0px 12px;
 }
-.tab {
+.flex-container > div {
+  padding: 0px 16px;
+  text-align: center;
+  font-size: 16px;
+  text-transform: uppercase;
+  transition-duration: 0.28s;
+  transition-property: box-shadow, transform, opacity;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+}
+.flex-container > div :hover {
+  cursor: pointer;
+  background-color: #d4d4d4;
+  opacity: 0.7;
+}
+/* avoid underline introduced by router link */
+.my-link {
   text-decoration: none;
+}
+nav li:hover,
+nav li.router-link-active,
+nav li.router-link-exact-active {
+  background-color: indianred;
+  cursor: pointer;
 }
 </style>
--- src/components/StockContainer.vue
+++ src/components/StockContainer.vue
@@ -1,16 +1,26 @@
 <template>
   <div>
-      this is stock container
+    this is stock container
+    <div>{{ fabric_stocks }}</div>
   </div>
 </template>
 
 <script>
-
+import axios from 'axios';
 export default {
-  name: 'StockContainer',
-  components: {
-  }
-}
+  name: "StockContainer",
+  components: {},
+  data() {
+    return {
+      fabric_stocks: [],
+    };
+  },
+  created() {
+    axios
+      .get("http://localhost:5000/fabrics")
+      .then((response) => (this.fabric_stocks = response));
+  },
+};
 </script>
 
 <style scoped>
